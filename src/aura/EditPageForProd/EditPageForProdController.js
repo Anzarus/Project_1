f74e8ab7d11bb39e5f612bf2f2e0f2cc0fe275dc
Anton Antoniuk <antonantonuk1999@gmail.com>
2019-08-20 17:03:02 +0300

    delete onerror function

--- src/aura/EditPageForProd/EditPageForProdController.js
+++ src/aura/EditPageForProd/EditPageForProdController.js
@@ -10,11 +10,5 @@
     handleSuccess: function (cmp, event, helper) {
         helper.showToast($A.get("$Label.c.success"), $A.get("$Label.c.ProdUpdated"), "success");
         $A.get('e.force:refreshView').fire();
-    },
-
-    handleError: function (cmp, event, helper) {
-        let errorMessage = event.getParam("message");
-        if (errorMessage === '') errorMessage = $A.get("$Label.c.GenErr");
-        helper.showToast($A.get("$Label.c.err"), errorMessage, 'error');
     }
 });
